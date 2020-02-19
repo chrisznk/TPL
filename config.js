b2af80fef8627dc09ab1017d492d525b77414cba
chrisznk <61246552+chrisznk@users.noreply.github.com>
2020-02-19 18:47:46 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,179 @@
+	// LISTE DES JOURS D ACTIVITE TPL //
+	var jour = []; 
+	jour[0] = ["j0", "Lundi"]; 
+	jour[1] = ["j1", "Mardi"];
+	jour[2] = ["j2", "Mercredi"];
+	jour[3] = ["j3", "Jeudi"];
+	jour[4] = ["j4", "Vendredi"];
+	jour[5] = ["j5", "Samedi"];
+	jour[6] = ["j6", "Dimanche"];
+	 
+	// LISTE DES CRENEAUX D ACTIVITE TPL//
+	var heure=[];
+	// Lundi
+	heure["j0"] = [];
+	heure["j0"][0] = ["j0h0", "Activité spécial 7h-9h"];
+	heure["j0"][1] = ["j0h1", "Matin"];
+	heure["j0"][2] = ["j0h2", "Après midi"];
+	 
+	//Mardi
+	heure["j1"]=[];
+	heure["j1"][0] = ["j1h0", "Activité spécial 7h-9h"];
+	heure["j1"][1] = ["j1h1", "Matin"];
+	heure["j1"][2] = ["j1h2", "Après midi"];
+	 
+	//Mercredi
+	heure["j2"] = [];
+	heure["j2"][0] = ["j2h0", "Activité spécial 7h-9h"];
+	heure["j2"][1] = ["j2h1", "Matin"];
+	heure["j2"][2] = ["j2h2", "Après midi"];
+	
+	//Jeudi
+	heure["j3"] = [];
+	heure["j3"][0] = ["j3h0", "Activité spécial 7h-9h"];
+	heure["j3"][1] = ["j3h1", "Matin"];
+	heure["j3"][2] = ["j3h2", "Après midi"];
+	
+	//Vendredi
+	heure["j4"] = [];
+	heure["j4"][0] = ["j4h0", "Activité spécial 7h-9h"];
+	heure["j4"][1] = ["j4h1", "Matin"];
+	heure["j4"][2] = ["j4h2", "Après midi"];
+	
+	//Samedi
+	heure["j5"] = [];
+	heure["j5"][0] = ["j5h0", "Activité spécial 7h-9h"];
+	heure["j5"][1] = ["j5h1", "Matin"];
+	heure["j5"][2] = ["j5h2", "Après midi"];
+	
+	//Dimanche
+	heure["j6"] = [];
+	heure["j6"][0] = ["j6h0", "Matin"];
+	heure["j6"][1] = ["j6h1", "Après midi"];
+
+
+
+	 
+	// POUR CHAQUE CRENEAU, LA LISTE DES SPOTS AUTORISéS//
+	var lieu = [];
+	//        LUNDI
+	//Activité spécial 7h-9h
+	lieu["j0h0"] = [];
+	lieu["j0h0"][0] = ["j0h0r0", "Lieu 1"];
+	lieu["j0h0"][1] = ["j0h0r1", "Lieu 2"];
+	//Matin
+	lieu["j0h1"] = [];
+	lieu["j0h1"][0] = ["j0h1r0", "Lieu 1"];
+	//Après midi
+	lieu["j0h2"] = [];
+	lieu["j0h2"][0] = ["j0h2r0", "Lieu 1"];
+	 
+	
+	//        MARDI
+	//Activité spécial 7h-9h
+	lieu["j1h0"] = [];
+	lieu["j1h0"][0] = ["j1h0r0", "Lieu 1"];
+	//Matin
+	lieu["j1h1"] = [];
+	lieu["j1h1"][0] = ["j1h1r0", "Lieu 1"];
+	//Après midi
+	lieu["j1h2"] = [];
+	lieu["j1h2"][0] = ["j1h2r0", "Lieu 1"];
+	
+	
+	//        MERCREDI
+	//Activité spécial 7h-9h
+	lieu["j2h0"] = [];
+	lieu["j2h0"][0] = ["j2h0r0", "Lieu 1"];
+	//Matin
+	lieu["j2h1"] = [];
+	lieu["j2h1"][0] = ["j2h1r0", "Lieu 1"];
+	//Après midi
+	lieu["j2h2"] = [];
+	lieu["j2h2"][0] = ["j2h2r0", "Lieu 1"];
+	
+	//        JEUDI
+	//Activité spécial 7h-9h
+	lieu["j3h0"] = [];
+	lieu["j3h0"][0] = ["j3h0r0", "Lieu 1"];
+	//Matin
+	lieu["j3h1"] = [];
+	lieu["j3h1"][0] = ["j3h1r0", "Lieu 1"];
+	//Après midi
+	lieu["j3h2"] = [];
+	lieu["j3h2"][0] = ["j3h2r0", "Lieu 1"];
+	
+	//        VENDREDI
+	//Activité spécial 7h-9h
+	lieu["j4h0"] = [];
+	lieu["j4h0"][0] = ["j4h0r0", "Lieu 1"];
+	//Matin
+	lieu["j4h1"] = [];
+	lieu["j4h1"][0] = ["j4h1r0", "Lieu 1"];
+	//Après midi
+	lieu["j4h2"] = [];
+	lieu["j4h2"][0] = ["j4h2r0", "Lieu 1"];
+	
+	//        SAMEDI
+	//Activité spécial 7h-9h
+	lieu["j5h0"] = [];
+	lieu["j5h0"][0] = ["j5h0r0", "Lieu 1"];
+	//Matin
+	lieu["j5h1"] = [];
+	lieu["j5h1"][0] = ["j5h1r0", "Lieu 1"];
+	//Après midi
+	lieu["j5h2"] = [];
+	lieu["j5h2"][0] = ["j5h2r0", "Lieu 1"];
+	
+	//        DIMANCHE
+	//Activité spécial 7h-9h
+	lieu["j6h0"] = [];
+	lieu["j6h0"][0] = ["j6h0r0", "Lieu 1"];
+	//Matin
+	lieu["j6h1"] = [];
+	lieu["j6h1"][0] = ["j6h1r0", "Lieu 1"];
+	//Après midi
+	lieu["j6h2"] = [];
+	lieu["j6h2"][0] = ["j6h2r0", "Lieu 1"];
+	
+	
+	
+	function filltheselect(liste, choix)
+	{switch (liste)
+	   {
+	   case "listejour":
+	      raz("listeheure");
+	      raz("listelieu");
+		  choix='j'+choix;
+		  //console.log(choix);
+		  
+	      for (i=0; i<heure[choix].length; i++)
+	         {
+	         new_option = new Option(heure[choix][i][1],heure[choix][i][0]);
+	         document.formu.elements["listeheure"].
+	 options[document.formu.elements["listeheure"].length]=new_option;
+	         }
+	      for (i=0; i<lieu[choix+"h0"].length; i++)
+	         {
+	         new_option = new Option(lieu[choix+"h0"][i][1],lieu[choix+"h0"][i][0]);
+	         document.formu.elements["listelieu"].options[document.formu.
+	 elements["listelieu"].length]=new_option;
+	         }
+	      break;
+	   case "listeheure":
+	      raz("listelieu");
+	      for (i=0; i<lieu[choix].length; i++)
+	         {
+	         new_option = new Option(lieu[choix][i][1],lieu[choix][i][0]);
+	         document.formu.elements["listelieu"].options[document.formu.
+	 elements["listelieu"].length]=new_option;
+	         }
+	      break;
+	   }
+	}
+	 
+	function raz(liste)
+	{l=document.formu.elements[liste].length;
+	for (i=l; i>=0; i--)
+	   document.formu.elements[liste].options[i]=null;
+	}
---
+++ script.js
@@ -0,0 +1,377 @@
+////////////////////////////////////////////////////////////////////////////////Code Login
+var NomUser="";
+var IdUser="";
+document.styleSheets[2].disabled = true;
+firebase.auth().signOut();
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+	  console.log(user);
+	  IdUser=user.uid;
+	  
+	  var jourDatabase = firebase.database().ref('user/'+IdUser);
+	  jourDatabase.on('value', function(snapshot) {
+			NomUser=snapshot.val();
+			
+			
+			//CECI EST LE PROCESSUS QUI VA RECEPTIONNER TOUTE ELS INFORMATIONS EN bdd
+		
+			var depannerConfirmation = "javascript:if(window.confirm(\'Voulez vous vous inscrire à ce créneau? Si vous avez un imprévu vous devrez vous organiser directement avec votre compagnon.\')){this.replaceWith( \'"+NomUser+"\' );};";
+			var BoutonDepanner='<a class="c-add o-btn js-event__add" onclick="'+depannerConfirmation+'"  href="javascript:;">Dépanner</a>';
+			var ListeDesCreneaux ='';
+
+
+			var jourDatabase = firebase.database().ref('calendrier');
+			jourDatabase.on('value', function(snapshot) {
+			  for(Jour in snapshot.val()){
+				  ListeDesCreneaux ='';
+				  var Equilibre="pair";
+				  var EquilibreMsg=": Toutes les associations sont complètes mais vous pouvez réserver un autre créneau. <br />";
+				//console.log(Jour);
+				for(Lieu in snapshot.child(Jour).val()){
+					ListeDesCreneaux+='<br /> <br /> • <u><b>'+Lieu+'</b></u> <br />';
+					//console.log(Lieu);
+					for(Heure in snapshot.child(Jour).child(Lieu).val()){
+						//console.log(Heure);
+						for(Frere1 in snapshot.child(Jour).child(Lieu).child(Heure).val()){
+							//console.log(Frere1);
+							var Frere2=snapshot.child(Jour).child(Lieu).child(Heure).child(Frere1).val();
+							//console.log(Jour+' '+Lieu+' '+Heure+' '+Frere1+' '+Frere2);
+
+							if(Frere2=="None"){
+								Frere2=BoutonDepanner;
+								Equilibre="impair";
+								EquilibreMsg=": Vous pouvez dépanner certains créneaux. <br />";
+							}
+							ListeDesCreneaux+='<b>'+Heure+'</b> : '+Frere1+' - '+Frere2+'<br />'
+
+						}
+					}
+				}
+				defaultEvents(Jour,
+							 EquilibreMsg,
+							ListeDesCreneaux,Equilibre);
+			  }
+			});	  
+			
+			
+			
+			
+      });
+
+    event.preventDefault();
+	 $('.formInput').fadeOut(500);
+	 $('.wrapperLogin').addClass('form-success');
+	 setTimeout(() => { 
+		
+		//document.getElementsByClassName("wrapperLogin")[0].style.visibility = "hidden"; 
+		
+		document.styleSheets[3].disabled = true;
+		document.styleSheets[2].disabled = false;
+		
+		//document.getElementsByClassName("wrapper")[0].style.visibility = "visible"; 
+	 }, 1000);
+  }
+});
+
+
+     
+
+
+
+
+
+$("#login-button").click(function(event){
+     
+
+	var email=document.getElementById("InputLogin").value+"";
+	var password=document.getElementById("InputPwd").value+"";
+  
+
+    //Sign In User with Email and Password
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		console.log(errorCode);
+		console.log(errorMessage);
+		if(errorMessage=="The email address is badly formatted."){
+			alert("Votre adresse mail n'est pas conforme.");
+		}
+		if(errorMessage=="There is no user record corresponding to this identifier. The user may have been deleted."){
+			alert("Votre adresse mail n'est pas enregistré. Veuillez vous rapprocher de votre responsable de prédication ou du préposé aux territoires.");
+		}
+		if(errorMessage=="The password is invalid or the user does not have a password."){
+			alert("Votre mot de passe est incorrecte. Veuillez reessayer.");
+		}
+		if(errorMessage=="The password is invalid or the user does not have a password."){
+			alert("Votre mot de passe est incorrecte. Veuillez reessayer.");
+		}
+		//alert('La connection a échoué. Verifiez votre adresse mail et mot de passe.');
+	});
+});
+
+////////////////////////////////////////////////////////////////////////////////Code Login
+
+
+//global variables
+var monthEl = $(".c-main");
+var dataCel = $(".c-cal__cel");
+var dateObj = new Date();
+var month = dateObj.getUTCMonth() + 1;
+var day = dateObj.getUTCDate();
+var year = dateObj.getUTCFullYear();
+var monthText = [
+  "Janvier",
+  "Fevrier",
+  "Mars",
+  "Avril",
+  "Mais",
+  "Juin",
+  "Juillet",
+  "Aout",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Decembre"
+];
+var indexMonth = month;
+var todayBtn = $(".c-today__btn");
+var addBtn = $(".js-event__add");
+var saveBtn = $(".js-event__save");
+var closeBtn = $(".js-event__close");
+var winCreator = $(".js-event__creator");
+var inputDate = $(this).data();
+today = year + "-" + month + "-" + day;
+
+
+// ------ set default events -------
+function defaultEvents(dataDay,dataName,dataNotes,classTag){
+  var date = $('*[data-day='+dataDay+']');
+  date.attr("data-name", dataName);
+  date.attr("data-notes", dataNotes);
+  date.addClass("event");
+  date.addClass("event--" + classTag);
+}
+
+//defaultEvents(today, 'YEAH!','Today is your day','aucun');
+//defaultEvents('2020-12-25', 'MERRY CHRISTMAS','A lot of gift!!!!','pair');
+//defaultEvents('2020-05-04', "LUCA'S BIRTHDAY",'Another gifts...?','impair');
+
+
+
+
+
+// ------ functions control -------
+
+//button of the current day
+todayBtn.on("click", function() {
+  if (month < indexMonth) {
+    var step = indexMonth % month;
+    movePrev(step, true);
+  } else if (month > indexMonth) {
+    var step = month - indexMonth;
+    moveNext(step, true);
+  }
+});
+
+//higlight the cel of current day
+dataCel.each(function() {
+  if ($(this).data("day") === today) {
+    $(this).addClass("isToday");
+    fillEventSidebar($(this));
+  }
+});
+
+//window event creator
+addBtn.on("click", function() {
+  winCreator.addClass("isVisible");
+  $("body").addClass("overlay");
+  dataCel.each(function() {
+    if ($(this).hasClass("isSelected")) {
+      today = $(this).data("day");
+      document.querySelector('input[type="date"]').value = today;
+	  
+	  filltheselect("listejour", new Date(today).getDay()-1);
+    } else {
+      document.querySelector('input[type="date"]').value = today;
+	  filltheselect("listejour", new Date(today).getDay()-1);
+    }
+  });
+});
+closeBtn.on("click", function() {
+  winCreator.removeClass("isVisible");
+  $("body").removeClass("overlay");
+});
+saveBtn.on("click", function() {
+  var inputName = $("input[name=name]").val();
+  var inputDate = $("input[name=listejour]").val();
+  var inputNotes = $("textarea[name=notes]").val();
+  var inputTag = $("select[name=tags]")
+    .find(":selected")
+    .text();
+
+  dataCel.each(function() {
+    if ($(this).data("day") === inputDate) {
+      if (inputName != null) {
+        $(this).attr("data-name", inputName);
+      }
+      if (inputNotes != null) {
+        $(this).attr("data-notes", inputNotes);
+      }
+      $(this).addClass("event");
+      if (inputTag != null) {
+        $(this).addClass("event--" + inputTag);
+      }
+      fillEventSidebar($(this));
+    }
+  });
+
+  winCreator.removeClass("isVisible");
+  $("body").removeClass("overlay");
+  $("#formu")[0].reset();
+});
+
+//fill sidebar event info
+function fillEventSidebar(self) {
+  $(".c-aside__event").remove();
+  var thisName = self.attr("data-name");
+  var thisNotes = self.attr("data-notes");
+  var thisImportant = self.hasClass("event--aucun");
+  var thisBirthday = self.hasClass("event--impair");
+  var thisFestivity = self.hasClass("event--pair");
+  var thisEvent = self.hasClass("event");
+  
+  switch (true) {
+    case thisImportant:
+      $(".c-aside__eventList").append(
+        "<p class='c-aside__event c-aside__event--aucun'>" +
+        thisName +
+        " <span> • " +
+        thisNotes +
+        "</span></p>"
+      );
+      break;
+    case thisBirthday:
+      $(".c-aside__eventList").append(
+        "<p class='c-aside__event c-aside__event--impair'>" +
+        thisName +
+        " <span>" +
+        thisNotes +
+        "</span></p>"
+      );
+      break;
+    case thisFestivity:
+      $(".c-aside__eventList").append(
+        "<p class='c-aside__event c-aside__event--pair'>" +
+        thisName +
+        " <span>" +
+        thisNotes +
+        "</span></p>"
+      );
+      break;
+    case thisEvent:
+      $(".c-aside__eventList").append(
+        "<p class='c-aside__event'>" +
+        thisName +
+        " <span>" +
+        thisNotes +
+        "</span></p>"
+      );
+      break;
+   }
+};
+dataCel.on("click", function() {
+  var thisEl = $(this);
+  var thisDay = $(this)
+  .attr("data-day")
+  .slice(8);
+  var thisMonth = $(this)
+  .attr("data-day")
+  .slice(5, 7);
+
+  fillEventSidebar($(this));
+
+  $(".c-aside__num").text(thisDay);
+  $(".c-aside__month").text(monthText[thisMonth - 1]);
+
+  dataCel.removeClass("isSelected");
+  thisEl.addClass("isSelected");
+
+});
+
+//function for move the months
+function moveNext(fakeClick, indexNext) {
+  for (var i = 0; i < fakeClick; i++) {
+    $(".c-main").css({
+      left: "-=100%"
+    });
+    $(".c-paginator__month").css({
+      left: "-=100%"
+    });
+    switch (true) {
+      case indexNext:
+        indexMonth += 1;
+        break;
+    }
+  }
+}
+function movePrev(fakeClick, indexPrev) {
+  for (var i = 0; i < fakeClick; i++) {
+    $(".c-main").css({
+      left: "+=100%"
+    });
+    $(".c-paginator__month").css({
+      left: "+=100%"
+    });
+    switch (true) {
+      case indexPrev:
+        indexMonth -= 1;
+        break;
+    }
+  }
+}
+
+//months paginator
+function buttonsPaginator(buttonId, mainClass, monthClass, next, prev) {
+  switch (true) {
+    case next:
+      $(buttonId).on("click", function() {
+        if (indexMonth >= 2) {
+          $(mainClass).css({
+            left: "+=100%"
+          });
+          $(monthClass).css({
+            left: "+=100%"
+          });
+          indexMonth -= 1;
+        }
+        return indexMonth;
+      });
+      break;
+    case prev:
+      $(buttonId).on("click", function() {
+        if (indexMonth <= 11) {
+          $(mainClass).css({
+            left: "-=100%"
+          });
+          $(monthClass).css({
+            left: "-=100%"
+          });
+          indexMonth += 1;
+        }
+        return indexMonth;
+      });
+      break;
+  }
+}
+
+buttonsPaginator("#next", monthEl, ".c-paginator__month", false, true);
+buttonsPaginator("#prev", monthEl, ".c-paginator__month", true, false);
+
+//launch function to set the current month
+moveNext(indexMonth - 1, false);
+
+//fill the sidebar with current day
+$(".c-aside__num").text(day);
+$(".c-aside__month").text(monthText[month - 1]);
