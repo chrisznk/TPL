2d971e01787e82e9e7f491652a864d2e97653887
chrisznk <61246552+chrisznk@users.noreply.github.com>
2020-02-29 15:17:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -425,6 +425,99 @@ function defaultEvents(dataDay,dataName,dataNotes,classTag){
 
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////MENU
+
+var BoutonMenuOpen = $(".BoutonMenuOpen");
+
+BoutonMenuOpen.on("click", function() {
+    if(document.getElementById('BoutonMenuOpen').checked){
+		document.getElementById('next').style.display = 'None';
+		document.getElementById('prev').style.display = 'None';
+		document.getElementById('TitreMuneNomUtilisateur').textContent=NomUser.LePrenom+" "+NomUser.LeNom;
+
+	}else{
+		document.getElementById('next').style.display = 'Block';
+		document.getElementById('prev').style.display = 'Block';
+
+	}
+});
+
+
+function onManageWebPushSubscriptionButtonClicked(event) {
+        getSubscriptionState().then(function(state) {
+            if (state.isPushEnabled) {
+                /* Subscribed, opt them out */
+                OneSignal.setSubscription(false);
+            } else {
+                if (state.isOptedOut) {
+                    /* Opted out, opt them back in */
+                    OneSignal.setSubscription(true);
+                } else {
+                    /* Unsubscribed, subscribe them */
+                    OneSignal.registerForPushNotifications();
+                }
+            }
+        });
+        event.preventDefault();
+    }
+
+    function updateMangeWebPushSubscriptionButton(buttonSelector) {
+        var hideWhenSubscribed = false;
+        var subscribeText = "Subscribe to Notifications";
+        var unsubscribeText = "Unsubscribe from Notifications";
+
+        getSubscriptionState().then(function(state) {
+            var buttonText = !state.isPushEnabled || state.isOptedOut ? subscribeText : unsubscribeText;
+
+            var element = document.querySelector(buttonSelector);
+            if (element === null) {
+                return;
+            }
+
+            element.removeEventListener('click', onManageWebPushSubscriptionButtonClicked);
+            element.addEventListener('click', onManageWebPushSubscriptionButtonClicked);
+            element.textContent = buttonText;
+
+            if (state.hideWhenSubscribed && state.isPushEnabled) {
+                element.style.display = "none";
+            } else {
+                element.style.display = "";
+            }
+        });
+    }
+
+    function getSubscriptionState() {
+        return Promise.all([
+          OneSignal.isPushNotificationsEnabled(),
+          OneSignal.isOptedOut()
+        ]).then(function(result) {
+            var isPushEnabled = result[0];
+            var isOptedOut = result[1];
+
+            return {
+                isPushEnabled: isPushEnabled,
+                isOptedOut: isOptedOut
+            };
+        });
+    }
+
+    var OneSignal = OneSignal || [];
+    var buttonSelector = "#my-notification-button";
+
+    /* This example assumes you've already initialized OneSignal */
+    OneSignal.push(function() {
+        // If we're on an unsupported browser, do nothing
+        if (!OneSignal.isPushNotificationsSupported()) {
+            return;
+        }
+        updateMangeWebPushSubscriptionButton(buttonSelector);
+        OneSignal.on("subscriptionChange", function(isSubscribed) {
+            /* If the user's subscription state changes during the page's session, update the button text */
+            updateMangeWebPushSubscriptionButton(buttonSelector);
+        });
+    });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////FIN MENU
 
 
 // ------ functions control -------
